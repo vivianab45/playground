3674e343ddce25751a655b69918ad82fec745965
vivianab45 <balleza45@gmail.com>
2023-07-07 15:22:52 -0500

    second commit

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template  # added render_template!
+from flask import Flask, render_template 
 app = Flask(__name__)                     
     
 @app.route('/play')                           
 def level1():
-    # Instead of returning a string, 
-    # we'll return the result of the render_template method, passing in the name of our HTML file
     return render_template('level1.html')  
 
 @app.route('/play/<int:num>')
@@ -14,10 +12,8 @@ def level2(num):
 
 @app.route('/play/<int:num>/<color>')
 def level3(num,color):
-    num= num
     color_change= color
     return render_template('level3.html', num=num, color_change=color)
-
 
 
 if __name__=="__main__":
